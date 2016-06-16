5cec2e37f824e4f03909994711b453b7d586ac14
nkakkireni <nkakkireni@spcapitaliq.com>
2016-06-16 12:08:04 +0530

    updated with 2 problems

---
+++ src/main/scala/com/fursa/arrays/MaxAbsDiffBetweenTwoSubArrays.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.fursa.arrays;
+
+/**
+ * @author nkakkireni
+ *
+ */
+public class MaxAbsDiffBetweenTwoSubArrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+		 
+		 
+		    findMaxAbsDiff(a);
+		 
+	}
+
+	private static void findMaxAbsDiff(int[] a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/scala/com/fursa/arrays/MaxCircularSum.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.fursa.arrays;
+
+/**
+ * @author nkakkireni
+ *
+ */
+public class MaxCircularSum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 int a[] =  {11, 10, -20, 5, -3, -5, 8, -13, 10};
+         System.out.println("Maximum circular sum is " +
+                         maxCircularSum(a));	}
+
+	private static String maxCircularSum(int[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
