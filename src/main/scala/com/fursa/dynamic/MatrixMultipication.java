e0998960154a1de373468c6553098ac4456b96ed
nkakkireni <nkakkireni@spcapitaliq.com>
2016-06-20 12:13:24 +0530

    updated

---
+++ src/main/scala/com/fursa/dynamic/MatrixMultipication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.fursa.dynamic;
+
+/**
+ * @author nkakkireni
+ *
+ */
+public class MatrixMultipication {
+
+	public static void main(String args[]){
+		MatrixMultipication mmc = new MatrixMultipication();
+		int arr[] = {4,2,3,5,3};
+		int cost = mmc.findCost(arr);
+		System.out.print(cost);
+	}
+
+	public int findCost(int arr[]){return 0;}
+
+}
