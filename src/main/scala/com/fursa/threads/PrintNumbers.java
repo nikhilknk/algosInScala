d139a79e7997ab4dbca8488d4899019268918666
Nikhil <nkakkireni@II01-JCHLG32.ciqhyd.com>
2016-08-04 15:09:24 +0530

    updated for odd even numbers printing

--- src/main/scala/com/fursa/threads/PrintNumbers.java
+++ src/main/scala/com/fursa/threads/PrintNumbers.java
@@ -1,54 +1,64 @@
 package com.fursa.threads;
+
 public class PrintNumbers extends Thread {
-volatile static int i = 1;
-//Object lock;
 
-PrintNumbers(Object lock) {
-  //  this.lock = lock;
-}
+	 Object lock;
 
-public static void main(String ar[]) {
-    Object obj = new Object();
-    // This constructor is required for the identification of wait/notify
-    // communication
-    PrintNumbers odd = new PrintNumbers(obj);
-    PrintNumbers even = new PrintNumbers(obj);
-    odd.setName("Odd");
-    even.setName("Even");
-    even.start();
-    odd.start();
-   
-}
+	PrintNumbers(Object lock) {
+		 this.lock = lock;
+	}
 
-@Override
-public void run() {
-    while (i <= 10) {
-        if (i % 2 == 0 && Thread.currentThread().getName().equals("Even")) {
-            synchronized(this)  {
-                System.out.println(Thread.currentThread().getName() + " - "
-                        + i);
-                i++;
-                /*try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }*/
-            }
-        }
-        if (i % 2 == 1 && Thread.currentThread().getName().equals("Odd")) {
-        	synchronized(this) {
-            	try {
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+	public static void main(String ar[]) {
+		Object obj = new Object();
+		// This constructor is required for the identification of wait/notify
+		// communication
+		PrintNumbers odd = new PrintNumbers(obj);
+		PrintNumbers even = new PrintNumbers(obj);
+		odd.setName("Odd");
+		even.setName("Even");
+		even.start();
+		odd.start();
+
+	}
+
+	@Override
+	public void run() {
+		for(int i=0;i<=100;i++) {
+
+			synchronized (lock) {
+
+				if (Thread.currentThread().getName().equals("Even")) {
+
+					if(i % 2 == 0 ){
+						System.out.println(Thread.currentThread().getName() + " - "+ i);
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}					
+					else if (i % 2 != 0 ) {
+						lock.notify();
+					}
 				}
-                System.out.println(Thread.currentThread().getName() + " - "
-                        + i);
-                i++;
-              // lock.notify();
-              }
-           }
-        }
-    }
+				
+				if (Thread.currentThread().getName().equals("Odd")) {
+
+					if(i % 2 == 1 ){
+						System.out.println(Thread.currentThread().getName() + " - "+ i);
+						try {
+							lock.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}					
+					else if (i % 2 != 1 ) {
+						lock.notify();
+					}
+				}
+				
+
+			}
+		}
+	}
 }
